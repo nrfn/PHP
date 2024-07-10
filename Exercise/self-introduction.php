81c67aa65432b1ca1d72e3df477b4059f062cc11
nrfn <59220732+nrfn@users.noreply.github.com>
2024-07-11 00:19:47 +0900

    self-introduction.php

---
+++ Exercise/self-introduction.php
@@ -0,0 +1,8 @@
+<?php
+    $name = 'nrfn';
+    $language = 'php';
+    $hitokoto = 'freelance';
+    echo "name:" . $name . PHP_EOL;
+    echo "language:" . $language . PHP_EOL;
+    echo "hitokoto:" . $hitokoto;
+?>
