0db1e4b64b48407afffbe7641b1589cb84d8edc9
nrfn <59220732+nrfn@users.noreply.github.com>
2024-07-15 23:58:40 +0900

    foreach,explodeの使い方を学習。

--- Exercise/stdin.php
+++ Exercise/stdin.php
@@ -33,3 +33,13 @@
         echo $input . PHP_EOL;
     }
 ?>
+
+<!-- N個のデータの入力 -->
+<?php
+    $input_line = fgets(STDIN);
+    $input_words = fgets(STDIN);
+    $array = explode(" ", $input_words);
+    foreach ($array as $word) {
+        echo $word . PHP_EOL;
+    }
+?>
